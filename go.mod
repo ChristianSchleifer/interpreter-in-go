785000de9e432288e67a778d5ff2db4cef4de397
Christian Schleifer <chr.schleifer@gmail.com>
2020-12-06 19:00:16 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ChristianSchleifer/interpreter-in-go
+
+go 1.15
---
+++ repl/repl.go
@@ -0,0 +1,40 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/ChristianSchleifer/interpreter-in-go/lexer"
+	"github.com/ChristianSchleifer/interpreter-in-go/parser"
+	"io"
+)
+
+// PROMPT gets printed at the beginning of each repl line
+const PROMPT = ">> "
+
+// Start starts up the repls
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
